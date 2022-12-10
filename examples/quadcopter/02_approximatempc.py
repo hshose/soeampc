957c1bd69795d5ebd2508bf321cf23e26f6ac1a8
Henrik Hose <henrik.hose@rwth-aachen.de>
2022-12-10 13:56:22 +0100

    approximate quadcopter architecture list

--- examples/quadcopter/02_approximatempc.py
+++ examples/quadcopter/02_approximatempc.py
@@ -9,46 +9,54 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..','..'))
 from pathlib import Path
 
+import fire
+
 fp = Path(os.path.dirname(__file__))
 os.chdir(fp)
 
 from soeampc import *
 
-# import latest dataset :-D
-mpc = import_mpc()
-X, U, _, _ = import_dataset(mpc)
+
+def approximatempc(dataset="latest"):
+    # import latest dataset :-D
+    mpc = import_mpc(dataset)
+    X, U, _, _ = import_dataset(mpc, dataset)
 
 
-# define architectures to be tested
-architectures = np.array([
-    # [mpc.nx, 10, mpc.nu*mpc.N],
-    # [mpc.nx, 20, mpc.nu*mpc.N],
-    # [mpc.nx, 40, mpc.nu*mpc.N],
-    # [mpc.nx, 80, mpc.nu*mpc.N],
-    # [mpc.nx, 160, mpc.nu*mpc.N],
-    # [mpc.nx, 10, 10, mpc.nu*mpc.N],
-    # [mpc.nx, 20, 20, mpc.nu*mpc.N],
-    # [mpc.nx, 40, 40, mpc.nu*mpc.N],
-    # [mpc.nx, 80, 80, mpc.nu*mpc.N],
-    # [mpc.nx, 160, 160, mpc.nu*mpc.N],
-    # [mpc.nx, 10, 20, mpc.nu*mpc.N],
-    # [mpc.nx, 20, 40, mpc.nu*mpc.N],
-    # [mpc.nx, 40, 80, mpc.nu*mpc.N],
-    # [mpc.nx, 80, 160, mpc.nu*mpc.N],
-    # [mpc.nx, 10, 10, 10, mpc.nu*mpc.N],
-    # [mpc.nx, 20, 20, 20, mpc.nu*mpc.N],
-    # [mpc.nx, 40, 40, 40, mpc.nu*mpc.N],
-    # [mpc.nx, 80, 80, 80, mpc.nu*mpc.N],
-    # [mpc.nx, 160, 160, 160, mpc.nu*mpc.N],
-    # [mpc.nx, 10, 20, 40, mpc.nu*mpc.N],
-    # [mpc.nx, 20, 40, 80, mpc.nu*mpc.N],
-    # [mpc.nx, 40, 80, 160, mpc.nu*mpc.N]
-    # [mpc.nx, 10, 100, 300, 600, mpc.nu*mpc.N]
-    [mpc.nx, 200, 400, 400, 400, 200, mpc.nu*mpc.N]
-    # [mpc.nx, 10, 20, 40, 60, 60, 60, mpc.nu*mpc.N],
-    # [mpc.nx, 10, 10, 10, 10, 10, 10, 10, 10, mpc.nu*mpc.N],
-    ])
+    # define architectures to be tested
+    architectures = np.array([
+        # [mpc.nx, 10, mpc.nu*mpc.N],
+        # [mpc.nx, 20, mpc.nu*mpc.N],
+        # [mpc.nx, 40, mpc.nu*mpc.N],
+        # [mpc.nx, 80, mpc.nu*mpc.N],
+        # [mpc.nx, 160, mpc.nu*mpc.N],
+        # [mpc.nx, 10, 10, mpc.nu*mpc.N],
+        # [mpc.nx, 20, 20, mpc.nu*mpc.N],
+        # [mpc.nx, 40, 40, mpc.nu*mpc.N],
+        # [mpc.nx, 80, 80, mpc.nu*mpc.N],
+        # [mpc.nx, 160, 160, mpc.nu*mpc.N],
+        # [mpc.nx, 10, 20, mpc.nu*mpc.N],
+        # [mpc.nx, 20, 40, mpc.nu*mpc.N],
+        # [mpc.nx, 40, 80, mpc.nu*mpc.N],
+        # [mpc.nx, 80, 160, mpc.nu*mpc.N],
+        # [mpc.nx, 10, 10, 10, mpc.nu*mpc.N],
+        # [mpc.nx, 20, 20, 20, mpc.nu*mpc.N],
+        # [mpc.nx, 40, 40, 40, mpc.nu*mpc.N],
+        # [mpc.nx, 80, 80, 80, mpc.nu*mpc.N],
+        # [mpc.nx, 160, 160, 160, mpc.nu*mpc.N],
+        # [mpc.nx, 10, 20, 40, mpc.nu*mpc.N],
+        # [mpc.nx, 20, 40, 80, mpc.nu*mpc.N],
+        # [mpc.nx, 40, 80, 160, mpc.nu*mpc.N]
+        # [mpc.nx, 10, 100, 300, 600, mpc.nu*mpc.N]
+        [mpc.nx, 200, 400, 400, 400, 200, mpc.nu*mpc.N]
+        # [mpc.nx, 10, 20, 40, 60, 60, 60, mpc.nu*mpc.N],
+        # [mpc.nx, 10, 10, 10, 10, 10, 10, 10, 10, mpc.nu*mpc.N],
+        ])
 
-# traverse list until architecture is found
-datasetname = "latest"
-model = hyperparametertuning(mpc, X, U, datasetname, architectures)
+    # traverse list until architecture is found
+    # datasetname = "latest"
+    model = hyperparametertuning(mpc, X, U, dataset, architectures)
+
+
+if __name__=="__main__":
+    fire.Fire(approximatempc)
