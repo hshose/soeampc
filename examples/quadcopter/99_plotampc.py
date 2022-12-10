ebeef548dc4bb2498979577ddbbdcf9af6e89529
Henrik Hose <henrik.hose@rwth-aachen.de>
2022-12-10 13:57:52 +0100

    quadcopter example plot function with fire

--- examples/quadcopter/99_plotampc.py
+++ examples/quadcopter/99_plotampc.py
@@ -18,27 +18,35 @@ from soeampc import *
 
 from plot import *
 
-dataset = "quadcopter_N_100000_20221129-171745"
+import fire
 
-mpc = import_mpc(dataset)
-X, U, _, _ = import_dataset(mpc, dataset)
-X_train, X_test, Y_train, Y_test = train_test_split(X, U, test_size=0.1, random_state=42)
-model = import_model()
+def plotampc(dataset='latest', model='latest'):
+    # dataset = "quadcopter_N_100000_20221129-171745"
 
-i = 1234
+    mpc = import_mpc(dataset)
+    X, U, _, _ = import_dataset(mpc, dataset)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, U, test_size=0.1, random_state=42)
+    model = import_model(model)
 
-x0 = X_test[i]
-U_MPC = Y_test[i]
-U_NN = model(x0).numpy()[0]
+    # i = 1234
 
-print("x0 =\n", x0, "\n")
-print("U_MPC =\n", U_MPC, "\n")
-print("U_NN =\n", U_NN, "\n")
+    for i in range(X_test.shape[0]):
+        x0 = X_test[i]
+        U_MPC = Y_test[i]
+        U_NN = model(x0).numpy()[0]
 
-X_MPC = mpc.forwardsim(x0, U_MPC)
-X_NN = mpc.forwardsim(x0, U_NN)
+        print("x0 =\n",     x0,     "\n")
+        print("U_MPC =\n",  U_MPC,  "\n")
+        print("U_NN =\n",   U_NN,   "\n")
 
-print("forwardsimcheck MPC:", mpc.feasible(X_MPC, U_MPC, verbose=True))
-print("forwardsimcheck NN:", mpc.feasible(X_NN, U_NN, verbose=True))
+        X_MPC = mpc.forwardsim(x0, U_MPC)
+        X_NN = mpc.forwardsim(x0, U_NN)
 
-plot_quadcopter_ol(mpc,[U_NN,U_MPC], [X_NN, X_MPC], labels=['NN','MPC'])
+        print("forwardsimcheck MPC:",   mpc.feasible(X_MPC, U_MPC,  verbose=True))
+        print("forwardsimcheck NN:",    mpc.feasible(X_NN,  U_NN,   verbose=True))
+
+        plot_quadcopter_ol(mpc,[U_NN,U_MPC], [X_NN, X_MPC], labels=['NN','MPC'])
+
+
+if __name__=='__main__':
+    fire.Fire(plotampc)
