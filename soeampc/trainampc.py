8a52713c7ffb1ee435530114786759106ba32cd7
Henrik Hose <henrik.hose@rwth-aachen.de>
2023-01-02 13:53:53 +0100

    comments and refactoring to fit google styleguide

--- soeampc/trainampc.py
+++ soeampc/trainampc.py
@@ -16,15 +16,19 @@ from datetime import datetime
 import os
 import errno
 
-from .utils import export_model
+from .datasetutils import export_model
 
-def import_model(datasetname="latest", modelname="latest"):
-    p = Path("models").joinpath(datasetname).joinpath(modelname)
+def import_model(dataset_name="latest", modelname="latest"):
+    """imports tensorflow keras model    
+    """
+    p = Path("models").joinpath(dataset_name).joinpath(modelname)
     model = keras.models.load_model(p)
     return model
 
-def export_model(model, datasetname, modelname):
-    p = Path("models").joinpath(datasetname)
+def export_model(model, dataset_name, modelname):
+    """exports tensorflow keras model
+    """
+    p = Path("models").joinpath(dataset_name)
     model.save(p.joinpath(modelname))
     link_name=p.joinpath("latest")
     target=modelname
@@ -38,6 +42,8 @@ def export_model(model, datasetname, modelname):
             raise e
 
 def clipped_mae(y_true, y_pred):
+    """clipped mae loss, WARNING: HARDCODED LIMITS!!!! DO NOT USE
+    """
     #   Args:
     #   y_true: Ground truth values. shape = `[batch_size, d0, .. dN]`.
     #   y_pred: The predicted values. shape = `[batch_size, d0, .. dN]`.
@@ -58,14 +64,29 @@ def clipped_mae(y_true, y_pred):
     # print(tf.abs(y_pred - y_true)[tf.math.logical_not(mask)])
     return backend.mean((tf.abs(y_pred - y_true)[tf.math.logical_not(mask)]), axis=-1)
 
-def mpccostmaewithslack(y_true, y_pred):
-    Jtrue = 0
-    Jpred = 0
-    e = 0
-    return Jpred + e - Jtrue
+# def mpc_cost_mae_with_slack(y_true, y_pred):
+#     Jtrue = 0
+#     Jpred = 0
+#     e = 0
+#     return Jpred + e - Jtrue
 
-def generatemodel(traindata, architecture, output_shape, clipped_mae=False):
+def generate_model(traindata, architecture, output_shape):
+    """returns a fully connected feedforward NN normalized to traindata
+    
+    Input layer is normalizer to traindata, hidden layers are tanh activation,
+    output layer is linear activation with N*nu neurons, where N is MPC horizon,
+    reshape layer is appended.
 
+    Args:
+        traindata:
+            numpy array of initial conditions, traindata.shape = (Nsamples, nx)
+        architecture:
+            array of layer widths: [nx, N_hidden_1, N_hidden_2, ..., N*nu]
+        output_shape:
+            shape_like of output, e.g. (N, nu)
+    Returns:
+        tensorflow keras model
+    """
     X_normalizer = layers.Normalization(input_shape=[architecture[0],], axis=None)
     X_normalizer.adapt(traindata)
 
@@ -80,37 +101,39 @@ def generatemodel(traindata, architecture, output_shape, clipped_mae=False):
     model.add(layers.Reshape(output_shape))
 
 
-    if clipped_mae:
+    # if clipped_mae:
 
-        def lam(x):
-            from keras import backend
-            ue      = 0.7853
-            umin = -ue
-            umax = 2-ue
-            return backend.clip(x, min_value=umin, max_value=umax)
+    #     def lam(x):
+    #         from keras import backend
+    #         ue      = 0.7853
+    #         umin = -ue
+    #         umax = 2-ue
+    #         return backend.clip(x, min_value=umin, max_value=umax)
         
-        model.add(Lambda(lam, input_shape=(None, 10), output_shape=(None, 10)))
-        loss=clipped_mae
-    else:
-        loss='mean_absolute_error'
-        # loss='mean_squared_error'
+    #     model.add(Lambda(lam, input_shape=(None, 10), output_shape=(None, 10)))
+    #     loss=clipped_mae
+    # else:
+        # loss='mean_absolute_error'
+    
+    loss='mean_squared_error'
 
-    initial_learning_rate = 0.01
-    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-            initial_learning_rate,
-            decay_steps=1000,
-            decay_rate=0.96,
-            staircase=True)
+    # initial_learning_rate = 0.01
+    # lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    #         initial_learning_rate,
+    #         decay_steps=1000,
+    #         decay_rate=0.96,
+    #         staircase=True)
  
 
-    def get_lr_metric(optimizer):
-        def lr(y_true, y_pred):
-            return optimizer._decayed_lr(tf.float32) # I use ._decayed_lr method instead of .lr
-        return lr
+    # def get_lr_metric(optimizer):
+    #     def lr(y_true, y_pred):
+    #         return optimizer._decayed_lr(tf.float32) # I use ._decayed_lr method instead of .lr
+    #     return lr
 
     # optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
     optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-    lr_metric = get_lr_metric(optimizer)
+    
+    # lr_metric = get_lr_metric(optimizer)
 
     model.compile(
         optimizer=optimizer,
@@ -119,13 +142,35 @@ def generatemodel(traindata, architecture, output_shape, clipped_mae=False):
 
     return model
 
-def trainmodel(model, X_train, Y_train, datasetname, batchsize=int(1e4), maxepochs=int(1e3), patience=int(1e3), learning_rate=1e-3):
+def train_model(model, X_train, Y_train, dataset_name, batch_size=int(1e4), max_epochs=int(1e3), patience=int(1e3), learning_rate=1e-3):
+    """trains a given model on X,Y dataset
+
+    Args:
+        model:
+            tensorflow keras model
+        X_train:
+            training data of initial conditions x0
+        Y_train:
+            training data of predicted input sequences
+        dataset_name:
+            used for saving the model with name it has been trained on
+        batch_size:
+            batch size used for learning
+        max_epochs:
+            number of epochs used for learning
+        patience:
+            if loss is not reduced for patience epochs, training is aborted
+        learning_rate:
+            learning_rate used for training
+    Returns:
+        trained tensorflow keras model
+    """
 
     backend.set_value(model.optimizer.learning_rate, learning_rate)
 
     overfitCallback = EarlyStopping(monitor='loss', min_delta=0, patience = patience)
 
-    checkpoint_filepath = Path("models").joinpath(datasetname, "checkpoint")
+    checkpoint_filepath = Path("models").joinpath(dataset_name, "checkpoint")
     model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_filepath,
         verbose=1,
@@ -138,29 +183,55 @@ def trainmodel(model, X_train, Y_train, datasetname, batchsize=int(1e4), maxepoc
         X_train,
         Y_train,
         verbose=2,
-        batch_size=batchsize,
-        epochs=maxepochs,
+        batch_size=batch_size,
+        epochs=max_epochs,
         validation_split = 0.1,
         callbacks=[overfitCallback, model_checkpoint_callback]
         )
     
     return model
 
-def retrainmodel(mpc, model, X, Y, architecturestring, datasetname, batchsize=int(1e4), maxepochs=int(1e3), patience=int(1e3), learning_rate=1e-3):
+def retrain_model(mpc, model, X, Y, architecture_string, dataset_name, batch_size=int(1e4), max_epochs=int(1e3), patience=int(1e3), learning_rate=1e-3):
+    """retrains a given model on X, Y dataset
+    
+    Args:
+        mpc:
+            mpc class object
+        model:
+            tensorflow keras class object
+        X:
+            initial conditions x0
+        Y:
+            input sequences corresponding to initial conditions x0
+        architecture_string:
+            string used for model saving
+        dataset_name:
+            string used for dataset saving
+        batch_size:
+            batch size used for learning
+        max_epochs:
+            number of epochs used for learning
+        patience:
+            if loss is not reduced for patience epochs, training is aborted
+        learning_rate:
+            learning_rate used for training
+    Returns:
+        tensorflow keras model
+    """
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
     print("X[42]", X_train[42])
-    model = trainmodel(model=model, X_train=X_train, Y_train=Y_train, datasetname=datasetname, batchsize=batchsize, maxepochs=maxepochs, patience=patience, learning_rate=learning_rate)
+    model = train_model(model=model, X_train=X_train, Y_train=Y_train, dataset_name=dataset_name, batch_size=batch_size, max_epochs=max_epochs, patience=patience, learning_rate=learning_rate)
     testresult, mu = statisticaltest(mpc, model, X_test, Y_test)
     date = datetime.now().strftime("%Y%m%d-%H%M%S")
-    modelname = architecturestring + '_mu=' + ('%.2f' % mu) + '_' + date
-    export_model(model, datasetname, modelname)
+    modelname = architecture_string + '_mu=' + ('%.2f' % mu) + '_' + date
+    export_model(model, dataset_name, modelname)
     return model
 
-def hyperparametertuning(mpc, X, Y, datasetname, architectures, maxepochs=int(1e5), patience=int(1e3), batchsize=int(1e4)):
+def hyperparametertuning(mpc, X, Y, dataset_name, architectures, max_epochs=int(1e5), patience=int(1e3), batch_size=int(1e4)):
     print("\nperforming hyperparameter tuning\n")
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
     output_shape=Y_train.shape[1:]
-    p = Path("models").joinpath(datasetname)
+    p = Path("models").joinpath(dataset_name)
     p.mkdir(parents=True,exist_ok=True)
     for a in architectures:
         if a[0] != mpc.nx:
@@ -173,18 +244,46 @@ def hyperparametertuning(mpc, X, Y, datasetname, architectures, maxepochs=int(1e
         model = generatemodel(X_train, a, output_shape)
         model.summary()
 
-        model = trainmodel(model, X_train, Y_train, datasetname, batchsize, maxepochs, patience)
+        model = train_model(model, X_train, Y_train, dataset_name, batch_size, max_epochs, patience)
 
         testresult, mu = statisticaltest(mpc, model, X_test, Y_test)
         date = datetime.now().strftime("%Y%m%d-%H%M%S")
         modelname = '-'.join([str(d) for d in a]) + '_mu=' + ('%.2f' % mu) + '_' + date
-        export_model(model, datasetname, modelname)
+        export_model(model, dataset_name, modelname)
         if testresult:
             return model
     return None
 
 
-def statisticaltest(mpc, model, testpoints_X, testpoints_V, p=int(10e3), Tf=20, Nol=10, delta_h=0.10, mu_crit=0.80):
+def statisticaltest(mpc, model, testpoints_X, testpoints_V, p=int(10e3), delta_h=0.10, mu_crit=0.80):
+    """Tests if model yields a yields a feasible solution to mpc in mu_crit fraction of testpoints_X
+    
+    Uses Hoeffdings inequality on indicator function I. If I(x0) = 1 iff model(x0) is a feasible solution to mpc problem.
+    mean(I) is compared to mu_crit using Hoeffdings inequality with confidence level delta_h.
+
+    Args:
+        mpc:
+            mpc class object, that should implement a method `mpc.forwardsim(x0,V)` and `mpc.feasible(X,V)`
+        model:
+            model class object, that should implement a call operator `model(x0).numpy()`.
+            This would typically be a keras / tensorflow model
+        testpoints_X:
+            numpy array of N samples of initial states x0.
+            testpoints_X.shape=(Nsamples,mpc.nx)
+        testpoints_V:
+            numpy array of N samples of input sequences corresponding to states x0.
+            testpoints_V.shape=(Nsamples, mpc.N, mpc.nu)
+        p:
+            number of samples to evaluate, p should be less than Nsamples
+        delta_h:
+            required confidence level
+        mu_crit:
+            required accuracy
+
+    Returns:
+        A tuple (passed, mu), where passed is boolean indicating if mu_crit is achieved and mu
+            is mean(I)
+    """
     p = min(np.shape(testpoints_X)[0], p)
     print("\noffline testing on \n\tp = ", p)
     I = np.zeros(p)
@@ -205,11 +304,11 @@ def statisticaltest(mpc, model, testpoints_X, testpoints_V, p=int(10e3), Tf=20, 
     epsilon = math.sqrt(-math.log(delta_h/2)/(2*p))
     print("\t epsilon =", epsilon)
 
-    worst_case_passing_dist = np.max(dist[I==1])
-    best_case_not_passing_dist = np.min(dist[I==0])
+    worst_case_passing_dist = np.max(dist[I==1],0)
+    best_case_not_passing_dist = np.min(dist[I==0],0)
 
-    print("\t worst_case_passing_dist =", worst_case_passing_dist)
-    print("\t best_case_not_passing_dist =", best_case_not_passing_dist)
+    print("\t worst case passing dist (I==1): V-Vtrue =", worst_case_passing_dist)
+    print("\t best case not passing dist (I==0): V-Vtrue =", best_case_not_passing_dist)
 
     if mu_crit <= mu - epsilon:
         print("test passed\n")
