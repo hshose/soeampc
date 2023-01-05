f64591e706a8c9f3f140106ca7c4e100fdb118b5
Henrik Hose <henrik.hose@rwth-aachen.de>
2023-01-05 17:10:27 +0100

    added statistical test to fire exposed functions

--- examples/stirtank_rmpc/20_approximatempc.py
+++ examples/stirtank_rmpc/20_approximatempc.py
@@ -14,7 +14,7 @@ os.chdir(fp)
 
 import fire
 
-from soeampc.trainampc import architecture_search, retrain_model, import_model
+from soeampc.trainampc import architecture_search, retrain_model, import_model, statistical_test
 from soeampc.mpcproblem import *
 from soeampc.datasetutils import import_dataset
 
@@ -47,17 +47,26 @@ def find_approximate_mpc(dataset="latest"):
     model = architecture_search(mpc, X, U, hyperparameters=hyperparameters, architectures=architectures)
     return model
 
-def retrain_ampc(datasetname="latest", modelname="latest", maxepochs = 5000, learningrate=1e-3):
+def retrain_ampc(dataset="latest", model_name="latest", max_epochs = int(1e3), learning_rate=1e-3, patience=int(1e3), batch_size=int(1e4)):
     # import latest dataset :-D
-    mpc = import_mpc(datasetname, MPCQuadraticCostLxLu)
-    X, Y, _, _ = import_dataset(mpc, datasetname)
+    mpc = import_mpc(dataset, MPCQuadraticCostLxLu)
+    X, Y, _, _ = import_dataset(mpc, dataset)
     
     # X_test, X_train, Y_test, Y_train = train_test_split(X, U, test_size=0.1, random_state=42)
-    model = import_model(modelname=modelname)
-    model = retrain_model(mpc=mpc, model=model, X=X, Y=Y, architecturestring=modelname.split('_',1)[0], maxepochs = maxepochs, learning_rate=learningrate)
+    model = import_model(modelname=model_name)
+    model = retrain_model(mpc=mpc, model=model, X=X, Y=Y, architecture_string=model_name.split('_',1)[0], max_epochs = max_epochs, learning_rate=learning_rate, batch_size=batch_size, patience=patience)
+
+def test_ampc(dataset="latest", model_name="latest", p=int(1e4)):
+    mpc = import_mpc(dataset, MPCQuadraticCostLxLu)
+    X, Y, _, _ = import_dataset(mpc, dataset)
+    
+    # X_test, X_train, Y_test, Y_train = train_test_split(X, U, test_size=0.1, random_state=42)
+    model = import_model(modelname=model_name)
+    statistical_test(mpc, model, X, Y, p=p)
 
 if __name__=="__main__":
     fire.Fire({
         "find_approximate_mpc": find_approximate_mpc,
         "retrain_ampc": retrain_ampc,
+        "test_ampc": test_ampc,
     })
