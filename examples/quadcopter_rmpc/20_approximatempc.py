0013f4c7e7df7e4c8536c53573dccd48218749b6
Henrik Hose <henrik.hose@rwth-aachen.de>
2023-01-06 14:59:02 +0100

    quad added hyperparameter list to ampc search

--- examples/quadcopter_rmpc/20_approximatempc.py
+++ examples/quadcopter_rmpc/20_approximatempc.py
@@ -14,10 +14,11 @@ import fire
 fp = Path(os.path.dirname(__file__))
 os.chdir(fp)
 
-from soeampc import *
+from soeampc.trainampc import architecture_search, retrain_model, test_ampc
+from soeampc.mpcproblem import *
+from soeampc.datasetutils import import_dataset
 
-
-def approximatempc(dataset="latest", maxepochs=int(1e6), batchsize=int(1e4)):
+def find_approximate_mpc(dataset="latest", maxepochs=int(1e6), batchsize=int(1e4)):
     # import latest dataset :-D
     mpc = import_mpc(dataset, MPCQuadraticCostLxLu)
     X, U, _, _ = import_dataset(mpc, dataset)
@@ -25,23 +26,27 @@ def approximatempc(dataset="latest", maxepochs=int(1e6), batchsize=int(1e4)):
 
     # define architectures to be tested
     architectures = np.array([
-        [mpc.nx, 200, 400, 600, 600, 400, 200, mpc.nu*mpc.N] # achieved mu=0.06
+        [mpc.nx, 200, 400, 400, 400, 200, mpc.nu*mpc.N] # achieved mu=0.06
+        # [mpc.nx, 200, 400, 600, 600, 400, 200, mpc.nu*mpc.N] # achieved mu=0.06
         # [mpc.nx, 200, 400, 600, 800, 600, 400, 200, mpc.nu*mpc.N]
         ])
 
     # traverse list until architecture is found
     # datasetname = "latest"
-    model = hyperparametertuning(mpc, X, U, dataset, architectures, batchsize=batchsize, maxepochs=maxepochs)
+    hyperparameters = [ {"learning_rate":0.01,   "patience": 1000, "max_epochs": 100000, "batch_size": 10000},
+                        {"learning_rate":0.005,  "patience": 1000, "max_epochs": 100000, "batch_size": 10000},
+                        {"learning_rate":0.002,  "patience": 1000, "max_epochs": 100000, "batch_size": 10000},
+                        {"learning_rate":0.001,  "patience": 1000, "max_epochs": 100000, "batch_size": 10000},
+                        {"learning_rate":0.0005, "patience": 1000, "max_epochs": 100000, "batch_size": 10000},
+                        {"learning_rate":0.0002, "patience": 1000, "max_epochs": 100000, "batch_size": 10000},
+                        {"learning_rate":0.0001, "patience": 1000, "max_epochs": 100000, "batch_size": 10000},]
 
-
-def retrainampc(datasetname="latest", modelname="latest", maxepochs = 5000, learningrate=1e-3):
-    # import latest dataset :-D
-    mpc = import_mpc(datasetname, MPCQuadraticCostLxLu)
-    X, Y, _, _ = import_dataset(mpc, datasetname)
-    
-    # X_test, X_train, Y_test, Y_train = train_test_split(X, U, test_size=0.1, random_state=42)
-    model = import_model(datasetname=datasetname, modelname=modelname)
-    model = retrainmodel(mpc=mpc, model=model, X=X, Y=Y, architecturestring=modelname.split('_',1)[0], datasetname=datasetname, maxepochs = maxepochs, learning_rate=learningrate)
+    model = architecture_search(mpc, X, U, hyperparameters=hyperparameters, architectures=architectures)
+    return model
 
 if __name__=="__main__":
-    fire.Fire()
+    fire.Fire({
+        "find_approximate_mpc": find_approximate_mpc,
+        "retrain_model": retrain_model,
+        "test_ampc": test_ampc,
+    })
