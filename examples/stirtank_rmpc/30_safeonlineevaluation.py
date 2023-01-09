0a21b3221a9bebcf0638d7d73cda1008848cd1cf
Henrik Hose <henrik.hose@rwth-aachen.de>
2023-01-09 11:35:41 +0100

    moved safe evaluation to package

--- examples/stirtank_rmpc/30_safeonlineevaluation.py
+++ examples/stirtank_rmpc/30_safeonlineevaluation.py
@@ -11,28 +11,21 @@ os.chdir(fp)
 
 import fire
 
-from soeampc.trainampc import import_model
-from soeampc.mpcproblem import *
-from soeampc.datasetutils import import_dataset
-from soeampc.safeonline import AMPC, SafeOnlineEvaluationAMPC, closed_loop_test
+from soeampc.safeonline import closed_loop_test_on_dataset
 
-def safe_online_vs_naive(dataset="latest", model="latest", N_samples=int(1e3)):
-    # import latest dataset :-D
-    mpc = import_mpc(dataset, MPCQuadraticCostLxLu)
-    X, U, _, _ = import_dataset(mpc, dataset)
-    if N_samples >= X.shape[0]:
-        N_samples = X.shape[0]
-        print("WARNING: N_samples exceeds size of dataset, will use N_samples =", N_samples,"instead")
-    # X_test, X_train, Y_test, Y_train = train_test_split(X, U, test_size=0.1, random_state=42)
-    model = import_model(modelname=model)
+# def evaluate_on_sampler(dataset="latest", model="latest", N_samples=int(1e3)):
+#     mpc, X, V, _, _ = mpc_dataset_import(dataset)
+#     sampler = ...
 
-    naive_controller = AMPC(mpc, model)
-    safe_controller = SafeOnlineEvaluationAMPC(mpc, model)
+#     model = import_model(modelname=model)
+    
+#     naive_controller = AMPC(mpc, model)
+#     safe_controller = SafeOnlineEvaluationAMPC(mpc, model)
 
-    controllers = [ naive_controller, safe_controller ]
-    closed_loop_test(X[:N_samples],U[:N_samples],controllers)
+#     controllers = [ naive_controller, safe_controller ]
+#     closed_loop_test(sampler, controllers)
 
 if __name__=="__main__":
     fire.Fire({
-        "safe_online_vs_naive": safe_online_vs_naive,
+        "closed_loop_test_on_dataset": closed_loop_test_on_dataset,
     })
